8912797e733f471dc3d8a745ad645b04e6f9f69e
NastyaDonskaya <87092466+NastyaDonskaya@users.noreply.github.com>
2025-05-15 14:49:06 +0300

    Update kpiRouter.js

--- server/routes/kpiRouter.js
+++ server/routes/kpiRouter.js
@@ -2,7 +2,9 @@ const Router = require('express')
 const router = new Router()
 const KpiController = require('../controllers/kpiController')
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/', checkRoleMiddleware('hr'), KpiController.create) 
+router.get('/vals/:userId', authMiddleware, KpiController.getValues)
 
 module.exports = router
